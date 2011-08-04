c2d6916a7f5f9dbe59329e719bb8e4f41102e934
vagmi <vagmi.mudumbai@gmail.com>
2011-08-04 10:40:10 +0530

    extracted functions from the loop

--- tasks.rb
+++ tasks.rb
@@ -1,11 +1,23 @@
 tasks = []
 
+def new_task(title)
+  {:title=>title}
+end
+
+def complete_task(task)
+  task[:status]=:completed
+end
+
+def tag_task(task,tag)
+  task[:tags] ||= []
+  task[:tags] << tag
+end
+
 1.upto(10) do |i|
-  t={:title=>"This is task #{i}"}
-  t[:status]="completed" if (rand*2).round==1
-  t[:tags]||=[]
-  t[:tags] << "tag1"
-  t[:tags] << "tag2" if (rand*2).round==1
+  t=new_task("This is task #{i}")
+  complete_task(t) if (rand*2).round==1
+  tag_task(t,"tag1")
+  tag_task(t,"tag2") if (rand*2).round==1
   tasks << t
 end
 
