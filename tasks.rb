0446f3a20d87355299b7b87278d8394552536637
vagmi <vagmi.mudumbai@gmail.com>
2011-08-04 10:51:07 +0530

    added persistence with YML to tasks

--- tasks.rb
+++ tasks.rb
@@ -1,3 +1,5 @@
+require 'yaml'
+
 tasks = []
 
 def new_task(title)
@@ -22,23 +24,25 @@ def complete_tasks_by_tag(tasks,tag)
   get_tasks_by_tag(tasks,tag).map { |t| complete_task(t) }
 end
 
-1.upto(10) do |i|
-  t=new_task("This is task #{i}")
-  complete_task(t) if (rand*2).round==1
-  tag_task(t,"tag1")
-  tag_task(t,"tag2") if (rand*2).round==1
-  tasks << t
+def persist(tasks)
+  f=File.open('tasks.yml','w')
+  f.write tasks.to_yaml
+  f.close
 end
 
-puts "Displaying all tasks"
-puts "-------------------------"
+def load
+  yml_content=File.read('tasks.yml')
+  YAML.load(yml_content)
+end
+
+#1.upto(10) do |i|
+  #t=new_task("This is task #{i}")
+  #complete_task(t) if (rand*2).round==1
+  #tag_task(t,"tag1")
+  #tag_task(t,"tag2") if (rand*2).round==1
+  #tasks << t
+#end
+
+tasks = load
 puts tasks
 
-
-puts "Displaying tasks tagged with 'tag2'"
-puts "-----------------------------------"
-puts get_tasks_by_tag(tasks,"tag2")
-
-puts "Displaying tasks tagged with 'tag2'"
-puts "-----------------------------------"
-puts complete_tasks_by_tag(tasks,"tag2")
