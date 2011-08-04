fe6d34973b6cb36311737dbd163859a06f24fe78
vagmi <vagmi.mudumbai@gmail.com>
2011-08-04 11:23:47 +0530

    refactored to class methods for querying and persistence

--- tasks.rb
+++ tasks.rb
@@ -1,8 +1,7 @@
 require 'yaml'
 
-tasks = []
-
 class Task
+  @@tasks = []
   attr_accessor :title, :status, :tags
   def initialize(title)
     @title=title
@@ -17,39 +16,34 @@ class Task
     @tags << tag_name
     self
   end
+
   def to_hashtags
     @tags.map {|t| "##{t}" }.join(" ")
   end
   def to_s
     "#{'[done]' if status==:completed} #{title} #{to_hashtags}"
   end
+
+  def self.by_tag(tag)
+    @@tasks.find_all { |t| t.tags.index(tag) }
+  end
+  def self.complete_by_tag(tag)
+    by_tag(tag).map { |t| t.complete }
+  end
+  def self.persist
+    f=File.open('tasks.yml','w')
+    f.write @@tasks.to_yaml
+    f.close
+  end
+  def self.load
+    yml_content=File.read('tasks.yml')
+    @@tasks=YAML.load(yml_content)
+  end
+  def self.tasks
+    @@tasks
+  end
 end
 
-def get_tasks_by_tag(tasks,tag)
-  tasks.find_all { |t| t[:tags].index(tag) }
-end
 
-def complete_tasks_by_tag(tasks,tag)
-  get_tasks_by_tag(tasks,tag).map { |t| complete_task(t) }
-end
-
-def persist(tasks)
-  f=File.open('tasks.yml','w')
-  f.write tasks.to_yaml
-  f.close
-end
-
-def load
-  yml_content=File.read('tasks.yml')
-  YAML.load(yml_content)
-end
-
-1.upto(10) do |i|
-  t=Task.new("This is task #{i}")
-  t.tag "tag1"
-  t.tag "tag2" if (rand*2).round==1
-  t.complete if (rand*2).round==1
-  tasks << t
-end
-
-puts tasks
+Task.load
+puts Task.tasks
